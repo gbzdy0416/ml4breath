6ec93f3f86de2add55ab984e3352e4c97a665c0f
gbzdy0416 <sareodcnp@gmail.com>
2025-03-27 21:48:05 +0100

    Error handling for converting.py

--- converting.py
+++ converting.py
@@ -3,18 +3,43 @@ import tensorflow as tf
 
 def convert_model(input_path='BreathingModel.h5', output_path='BreathingModel.mlmodel', input_shape=(1,3)):
     # Loading Model
-    model = tf.keras.models.load_model(input_path, compile=False)
+    print("Loading Model...")
+    if not os.path.exists(input_path):
+        print(f"File does not exist：{input_path}")
+        sys.exit(1)
+
+
+    try:
+        model = tf.keras.models.load_model(input_path, compile=False)
+    except Exception as e:
+        print(f"Model loading failed: {e}")
+        sys.exit(1)
+
 
     # Converting into MLModel
-    mlmodel = ct.convert(
-              model,
-              source="tensorflow",
-              inputs=[ct.TensorType(input_shape)],
-              minimum_deployment_target=ct.target.iOS14
-    )
+    print("Start converting...")
+    try:
+        mlmodel = ct.convert(
+                  model,
+                  source="tensorflow",
+                  inputs=[ct.TensorType(input_shape)],
+                  minimum_deployment_target=ct.target.iOS14
+        )
 
-    # Saving the model in MLModel format
-    mlmodel.save(output_path)
+        # Saving the model in MLModel format
+
+        try:
+            print("Converting successful.")
+            mlmodel.save(output_path)
+            print("The mlmodel format model is saved.")
+
+        except Exception as e:
+            print(f"Saving failed: {e}")
+        sys.exit(1)
+    except Exception as e:
+        print(f"Converting failed: {e}")
+        sys.exit(1)
+
 
 if __name__ == "__main__":
     convert_model()
