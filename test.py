ce31815d2312c108c5eafcd194c9f791f976e20d
gbzdy <sareodcnp@gmail.com>
2025-03-27 19:27:44 +0100

    default values added for avoiding nulls

--- test.py
+++ test.py
@@ -1,3 +1,20 @@
+import numpy as np
 import tensorflow as tf
-print(tf.__file__)
-print(tf.keras)
+from sklearn.preprocessing import MinMaxScaler
+
+# Loading Model
+model = tf.keras.models.load_model('breath.h5')
+
+# Modification of test data
+
+input_data = np.array([[0, 1, 0.2]])
+
+# Prediction
+prediction = model.predict(input_data)
+
+# Result
+tick_length, inhale,  exhale, repetition = prediction[0]
+print(f"Tick length: {tick_length:.2f}")
+print(f"Inhale: {inhale:.2f} ticks")
+print(f"Exhale: {exhale:.2f} ticks")
+print(f"Repetition: {repetition:.0f} times")
--- training.py
+++ training.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+import numpy as np
 
 #Loading data
 
@@ -9,24 +11,30 @@ data = pd.read_csv('things.csv',
                           "tick_length", "inhale", "hold", "exhale", "repetition"])
 
 #Setting training set
-
-input_set = data[["function", "intensity_function", "intensity_time"]].values
-output_set = data[["tick_length", "inhale", "hold", "exhale", "repetition"]].values
+scaler = MinMaxScaler()
+input_set = scaler.fit_transform(data[["function", "intensity_function", "intensity_time"]].values)
+#Hold is deleted because almost nobody in questionnaire did it...
+output_set = data[["tick_length", "inhale", "exhale", "repetition"]].values
 input_train, input_test, output_train, output_test = train_test_split(input_set, output_set,
                                                                       test_size=0.2, random_state=42)
+
+#Setting Weight of Data
+sample_weight = np.ones(len(input_train))
+#Default test points are based on HRV breath
+sample_weight[:66] = 17.0
 #Setting and Training Model
 
 model = tf.keras.Sequential([
     tf.keras.layers.Dense(64, activation='relu', input_shape=(input_train.shape[1],)),
     tf.keras.layers.Dense(64, activation='relu'),
-    tf.keras.layers.Dense(5)
+    tf.keras.layers.Dense(4, activation='relu')
 ])
 model.compile(
     optimizer='adam',
     loss='mse',
     metrics=['mae']
 )
-model.fit(input_train, output_train, epochs=50, batch_size=16, validation_data=(input_test, output_test))
+model.fit(input_train, output_train, sample_weight=sample_weight, epochs=50, batch_size=16, validation_data=(input_test, output_test))
 
 #Converting into Concrete Function
 
@@ -36,3 +44,4 @@ concrete_func = run_model.get_concrete_function(
 
 #Saving Model
 model.save('breath.h5')
+
